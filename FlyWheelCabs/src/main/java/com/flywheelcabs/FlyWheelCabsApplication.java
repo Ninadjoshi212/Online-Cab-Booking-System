f1e5fbf01d0ce0566c02640be4574afcbb7d356f
jagatjit barik <itsjagatjitbarik@gmail.com>
2022-11-08 23:26:16 +0530

    initializing skeliton

---
+++ FlyWheelCabs/src/main/java/com/flywheelcabs/FlyWheelCabsApplication.java
@@ -0,0 +1,13 @@
+package com.flywheelcabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyWheelCabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyWheelCabsApplication.class, args);
+	}
+
+}
---
+++ FlyWheelCabs/src/test/java/com/flywheelcabs/FlyWheelCabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flywheelcabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyWheelCabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

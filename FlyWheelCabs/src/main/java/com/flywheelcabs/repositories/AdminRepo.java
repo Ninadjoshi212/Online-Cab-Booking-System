a1cef2c60fce341c5e20d8eea00aeb446e02de93
PrathmeshPshelar <prathmeshshelar13999@gmail.com>
2022-11-09 23:06:32 +0530

    fw18_0595-day02 added

---
+++ FlyWheelCabs/src/main/java/com/flywheelcabs/repositories/AdminRepo.java
@@ -0,0 +1,11 @@
+package com.flywheelcabs.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.flywheelcabs.modules.Admin;
+
+@Repository
+public interface AdminRepo extends JpaRepository<Admin, Integer>{
+
+}

631ca226bb4edf6d8b33678f0bcf5d44c4dfc602
aniket427 <bhandarkaraniket0@gmail.com>
2022-11-09 23:55:14 +0530

    day 2 work added

--- FlyWheelCabs/src/main/java/com/flywheelcabs/controllers/CabController.java
+++ FlyWheelCabs/src/main/java/com/flywheelcabs/controllers/CabController.java
@@ -1,12 +1,27 @@
 package com.flywheelcabs.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.flywheelcabs.modules.Cab;
 import com.flywheelcabs.services.CabServices;
 
+
+@RestController
 public class CabController {
 
-	private CabServices cabService;
+	private CabServices cabService; //cabService taken from service layer
 	
 	
+//	Insert cab presentation layer 
+	
+	@PostMapping("/insertcab")
+	  public ResponseEntity<Cab> insertCabHandler(@RequestBody Cab cab){
+		  Cab c=cabService.insertCab(cab);
+		  return new ResponseEntity<Cab>(c,HttpStatus.ACCEPTED);
+		  
+	  }
 }

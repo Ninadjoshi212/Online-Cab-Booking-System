cdabf472fd05a2a1e5939bf8dddd1f304ab51de1
aniket427 <bhandarkaraniket0@gmail.com>
2022-11-09 23:07:46 +0530

    Cab entity, cab service, cab controller created

---
+++ FlyWheelCabs/src/main/java/com/flywheelcabs/controllers/CabController.java
@@ -0,0 +1,12 @@
+package com.flywheelcabs.controllers;
+
+import org.springframework.http.ResponseEntity;
+
+import com.flywheelcabs.services.CabServices;
+
+public class CabController {
+
+	private CabServices cabService;
+	
+	
+}

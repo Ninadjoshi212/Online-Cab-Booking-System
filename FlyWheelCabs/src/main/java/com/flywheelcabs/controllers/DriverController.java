376b26ece5827ec6493aa8ef9e8f261029087985
Uditshetty <udit123.uv@gmail.com>
2022-11-11 23:43:31 +0530

    adding rest of the driver controller functionality

--- FlyWheelCabs/src/main/java/com/flywheelcabs/controllers/DriverController.java
+++ FlyWheelCabs/src/main/java/com/flywheelcabs/controllers/DriverController.java
@@ -1,13 +1,18 @@
 package com.flywheelcabs.controllers;
 
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,61 +30,39 @@ public class DriverController {
 	private DriverService ds;
 	
 	@PostMapping("/drivers")
-	public ResponseEntity<Driver> resgisterEmployeeHandler(@Valid @RequestBody Driver driver) throws DriverException{
+	public ResponseEntity<Driver> resgisterDriverHandler(@Valid @RequestBody Driver driver) throws DriverException{
 		Driver dr= ds.registerDriver(driver);
 		
 		return new ResponseEntity<Driver>(dr,HttpStatus.CREATED);
 	}
 	
-//	@GetMapping("/employees/{empid}")
-//	public ResponseEntity<Employee> getEmployeeByIdHandler(@PathVariable("empid") Integer empId) throws EmployeeException{
-//		Employee emp=es.getEmployeeById(empId);
-//		
-//		return new ResponseEntity<Employee>(emp,HttpStatus.OK);
-//	}
-//	
-//	@GetMapping("/getallemployees")
-//	public ResponseEntity<List<Employee>> getAllEmployeeDetails() throws EmployeeException{
-//		List<Employee> emp=es.getAllEmployeeDetails();
-//		
-//		
-//		return new ResponseEntity<List<Employee>>(emp,HttpStatus.OK);
-//	}
-//	
-//	@DeleteMapping("/deleteemployees/{empid}")
-//	public ResponseEntity<Employee> deleteEmployeeById(@PathVariable("empid") Integer empId) throws EmployeeException{
-//		
-//		Employee emp=es.deleteEmployeeById(empId);
-//		return new ResponseEntity<Employee>(emp, HttpStatus.OK);
-//	}
-//	
-//	@GetMapping("/getemployeebyaddress/address")
-//	public ResponseEntity<List<Employee>>  getEmployeeDetailsByAddress(@PathVariable("address") String address) throws EmployeeException{
-//		
-//		List<Employee> list=es.getEmployeeDetailsByAddress(address);
-//		
-//		return new ResponseEntity<List<Employee>>(list,HttpStatus.ACCEPTED);
-//	}
-//	
-//	@PutMapping("/employees/{empid}")
-//	public ResponseEntity<Employee> updateEmployee(@PathVariable("empid") Employee emp) throws EmployeeException{
-//		Employee em=es.updateEmployee(emp);
-//		
-//		return new ResponseEntity<Employee>(em,HttpStatus.OK);
-//	}
-//	
-//	@GetMapping("/getnameaddess/empid")
-//	public ResponseEntity<List<String>> getNameAndAddressOfEmplyeeById(@PathVariable("empid") Integer empId) throws EmployeeException{
-//		String result=es.getNameAndAddressOfEmplyeeById(empId);
-//		
-//		return new ResponseEntity<List<String>>(HttpStatus.OK);
-//	}
-//	
-//	@GetMapping("/getnameAddressSalary")
-//	public ResponseEntity<List<EmployeeDTO>> getNameAddressSalaryOfAllEmployee() throws EmployeeException{
-//		List<EmployeeDTO> led=es.getNameAddressSalaryOfAllEmployee();
-//		
-//		return new ResponseEntity<List<EmployeeDTO>>(led,HttpStatus.OK);
-//		
-//	}
+	@GetMapping("/drivers/{driverId}")
+	public ResponseEntity<Driver> getDriverByIdHandler(@PathVariable("driverId") Integer driverId) throws DriverException{
+		Driver dr=ds.viewBestDriver(driverId);
+		
+		return new ResponseEntity<Driver>(dr,HttpStatus.OK);
+	}
+	
+	@GetMapping("/getalldrivers")
+	public ResponseEntity<List<Driver>> getAllEmployeeDetails() throws DriverException{
+		List<Driver> dr=ds.getAllDriver();
+		
+		
+		return new ResponseEntity<List<Driver>>(dr,HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deletedrivers/{driverId}")
+	public ResponseEntity<Driver> deleteEmployeeById(@PathVariable("driverId") Integer driverId) throws DriverException{
+		
+		Driver dr=ds.removeDriver(driverId);
+		return new ResponseEntity<Driver>(dr, HttpStatus.OK);
+	}
+	
+	@PutMapping("/drivers/{driverId}")
+	public ResponseEntity<Driver> updateEmployee(@PathVariable("driverId") Driver driver) throws DriverException{
+		Driver em=ds.updateDriver(driver);
+		
+		return new ResponseEntity<Driver>(em,HttpStatus.OK);
+	}
+	
 }

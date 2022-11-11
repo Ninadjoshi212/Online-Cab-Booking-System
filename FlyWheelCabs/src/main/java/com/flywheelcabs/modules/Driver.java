c2bb8d583bd4c62a42dbd61bcea890a0db671f3b
Uditshetty <udit123.uv@gmail.com>
2022-11-11 14:32:23 +0530

    debugging error

--- FlyWheelCabs/src/main/java/com/flywheelcabs/modules/Driver.java
+++ FlyWheelCabs/src/main/java/com/flywheelcabs/modules/Driver.java
@@ -1,44 +1,46 @@
-//package com.flywheelcabs.modules;
-//
-//import javax.persistence.CascadeType;
-//import javax.persistence.Column;
-//import javax.persistence.Embedded;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.OneToOne;
-//import javax.validation.constraints.NotNull;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.EqualsAndHashCode;
-//import lombok.NoArgsConstructor;
-//import lombok.ToString;
-//
-//@Data
-//@EqualsAndHashCode(callSuper = true)
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@ToString
-//@Entity
-//public class Driver extends AbstractUser{
-//
-//		@Id
-//		@GeneratedValue(strategy = GenerationType.AUTO) // Auto Generation/Increment of Cutomer-Id
-//		private Integer driverId;
-//		
-//		@Column(unique = true)
-//		@NotNull
-//		private String licenceNo;
-//		
-////		@OneToOne(cascade = CascadeType.ALL)
-////		private Cab cab;
-//		
-//		private float perkmRate;
-//		
-////		@Embedded
-////		private AbstractUser abstractuser;
-//
-//
-//}
+package com.flywheelcabs.modules;
+
+import javax.persistence.Column;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Driver extends AbstractUser{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer driverId;
+	
+	
+	 
+	
+	private Float rating;
+	
+	@Column(unique = true)
+	@NotNull
+	private String licenceNo;
+	
+	
+	
+//	@Embedded
+//	private AbstractUser user;
+	
+//	@OneToOne(cascade = CascadeType.ALL)
+//	private Cab cab;
+
+}
--- FlyWheelCabs/src/main/java/com/flywheelcabs/services/DriverServiceImpl.java
+++ FlyWheelCabs/src/main/java/com/flywheelcabs/services/DriverServiceImpl.java
@@ -1,93 +1,89 @@
-//package com.flywheelcabs.services;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import com.flywheelcabs.exceptions.DriverException;
-//import com.flywheelcabs.modules.Driver;
-//import com.flywheelcabs.repositories.DriverDAO;
-////import com.flywheelcabs.repositories.DriverSessionDao;
-//
-//
-//@Service
-//public class DriverServiceImpl implements DriverService {
-//
+package com.flywheelcabs.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.flywheelcabs.exceptions.DriverException;
+import com.flywheelcabs.modules.Driver;
+import com.flywheelcabs.repositories.DriverDAO;
+
+import java.util.Optional;
+
+
+
+
+
+
+
+
+@Service
+public class DriverServiceImpl implements DriverService {
+
+	@Autowired
+	DriverDAO driverDao;
+
 //	@Autowired
-//	private DriverDAO driverDao;
-//
-////	@Autowired
-////	private DriverSessionDao driverSessionDao;
-//
-//	@Override
-//	public Driver insertDriver(Driver driver) throws DriverException {
-//		Driver ddriver=driverDao.save(driver);
-//		if(ddriver!=null) {
-//			return ddriver;
-//		}
-//		else
-//		throw new DriverException("Driver detail doesn't exist");
-//	}
-//
-//	@Override
-//	public Driver updateDriver(Driver driver) throws DriverException {
-//		Optional<Driver> opt=driverDao.findById(driver.getDriverId());
-//		if(opt.isPresent()) {
-//			 Driver updateDriver=driverDao.save(driver);
-//			 return updateDriver;
-//		}
-//		else
-//			throw new DriverException("Invalid Driver Detail");
-//	}
-//
-//	@Override
-//	public Driver deleteDriver(Integer driverId) throws DriverException {
-//		Optional<Driver> opt=driverDao.findById(driverId);
-//		
-//		if(opt.isPresent()) {
-//			Driver existingemployee=opt.get();
-//			driverDao.delete(existingemployee);
-//			
-//			return existingemployee;
-//		}
-//		else
-//			throw new DriverException("Driver does not exist with this driverId");
-//	}
-//
-//	@Override
-//	public List<Driver> viewBestDriver(String key, float rating) throws DriverException {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public String logoutDriver(String key) throws DriverException {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public List<Driver> getAllDriver(Integer driverId) throws DriverException {
-//		List<Driver> dr=driverDao.findAll();
-//		
-//		if(dr.size()==0) {
-//			throw new DriverException("No driver detail is there");
-//		}
-//		else 
-//			return dr;
-//	}
-//
-//	@Override
-//	public Driver viewDriver(Integer driverid) throws DriverException {
-//		return driverDao.findById(driverid).orElseThrow(()-> new DriverException("Invalid Driver id"));
-//
-//	}
-//
-//	
-//
-//	
-//	
-//	
-//}
+//	DriverSessionDao driverSessionDao;
+
+	@Override
+	public Driver registerDriver(Driver driver) throws DriverException {
+		Driver dr=driverDao.save(driver);
+		if(dr!=null){
+			return dr;
+		}
+		else 
+			throw new DriverException("Driver not found");
+	}
+
+	@Override
+	public List<Driver> getAllDriver() throws DriverException {
+		List<Driver> dr=driverDao.findAll();
+		
+		if(dr.size()==0) {
+			throw new DriverException("No Driver deatil is there");
+		}
+		else 
+			return dr;
+	}
+
+	@Override
+	public Driver removeDriver(Integer driverId) throws DriverException {
+		Optional<Driver> opt=driverDao.findById(driverId);
+		
+		if(opt.isPresent()) {
+			Driver existingdriver=opt.get();
+			driverDao.delete(existingdriver);
+			
+			return existingdriver;
+		}
+		else
+			throw new DriverException("Driver does not exist with this driverId");
+	}
+
+	@Override
+	public Driver updateDriver(Driver driver) throws DriverException {
+		Optional<Driver> opt=driverDao.findById(driver.getDriverId());
+		if(opt.isPresent()) {
+			 Driver updateDriver=driverDao.save(driver);
+			 return updateDriver;
+		}
+		else
+			throw new DriverException("Invalid Driver Detail");
+	}
+
+	@Override
+	public Driver viewBestDriver(Integer driverId) throws DriverException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+	
+	
+
+	
+
+}

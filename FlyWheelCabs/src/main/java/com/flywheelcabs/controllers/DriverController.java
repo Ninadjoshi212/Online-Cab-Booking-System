806a6a135848d118522847c45240ecb322b98967
Uditshetty <udit123.uv@gmail.com>
2022-11-13 10:07:44 +0530

    adding some more functionality

--- FlyWheelCabs/src/main/java/com/flywheelcabs/controllers/DriverController.java
+++ FlyWheelCabs/src/main/java/com/flywheelcabs/controllers/DriverController.java
@@ -14,60 +14,110 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.flywheelcabs.exceptions.AdminException;
 import com.flywheelcabs.exceptions.DriverException;
+import com.flywheelcabs.exceptions.LoginException;
 import com.flywheelcabs.modules.Driver;
 import com.flywheelcabs.services.DriverService;
+import com.flywheelcabs.services.LoginService;
+import com.flywheelcabs.services.TripServices;
 
 
 
-
+@RequestMapping("/drivers")
 @RestController
 public class DriverController {
 
 	@Autowired
 	private DriverService dservice;
 	
-	@PostMapping("/drivers")
+	@Autowired
+	private LoginService lService;
+
+	@Autowired
+	private TripServices tService;
+	
+	@PostMapping("/register")
 	public ResponseEntity<Driver> resgisterDriverHandler(@Valid @RequestBody Driver driver) throws DriverException{
 		Driver dr= dservice.registerDriver(driver);
 		
 		return new ResponseEntity<Driver>(dr,HttpStatus.CREATED);
 	}
 	
+	//### Get single customer using CustomerId
 	@GetMapping("/drivers/{driverId}")
-	public ResponseEntity<Driver> getDriverByIdHandler(@PathVariable("driverId") Integer driverId) throws DriverException{
+	public ResponseEntity<Driver> getDriverByIdHandler(@PathVariable("driverId") Integer driverId) 
+			throws DriverException,AdminException{
 		Driver dr=dservice.viewBestDriver(driverId);
 		
 		return new ResponseEntity<Driver>(dr,HttpStatus.OK);
 	}
 	
+	//### Get single Driver using Mobile ###
+		@GetMapping("/mobile/{mobileNum}")
+		public ResponseEntity<Driver> getDriverByMobileHandler(@PathVariable("mobileNum") String mobile)
+				throws DriverException, AdminException {
+
+			Driver fetchedDriver = dservice.getDriverByMobile(mobile);
+
+			return new ResponseEntity<Driver>(fetchedDriver, HttpStatus.OK);
+		}
+
+	//### Validate customer ###
+		@GetMapping("/validate/{userName}")
+		public ResponseEntity<Driver> validateDriverHandler(@PathVariable("userName") String userName,
+				@RequestParam("password") String password) throws DriverException, AdminException {
+
+			Driver validatedDriver = dservice.validateDriver(userName, password);
+
+			return new ResponseEntity<Driver>(validatedDriver, HttpStatus.OK);
+		}
+
+	//### Get All customer in a List ###
 	@GetMapping("/getalldrivers")
-	public ResponseEntity<List<Driver>> getAllEmployeeDetails() throws DriverException{
+	public ResponseEntity<List<Driver>> getAllDriversDetailsHandler() throws DriverException,AdminException{
 		List<Driver> dr=dservice.getAllDriver();
 		
 		
 		return new ResponseEntity<List<Driver>>(dr,HttpStatus.OK);
 	}
 	
+	//### Delete Account ###
 	@DeleteMapping("/deletedrivers/{driverId}")
-	public ResponseEntity<Driver> deleteEmployeeById(@PathVariable("driverId") Integer driverId) throws DriverException{
+	public ResponseEntity<Driver> deleteEmployeeById(@PathVariable("driverId") Integer driverId) 
+										throws DriverException,LoginException{
 		
 		Driver dr=dservice.removeDriver(driverId);
 		return new ResponseEntity<Driver>(dr, HttpStatus.OK);
 	}
 	
 	@PutMapping("/drivers/{driverId}")
-	public ResponseEntity<Driver> updateEmployee(@PathVariable("driverId") Driver driver) throws DriverException{
+	public ResponseEntity<Driver> updateDriverHandler(@PathVariable("driverId") Driver driver) 
+			throws DriverException,LoginException{
 		Driver em=dservice.updateDriver(driver);
 		
 		return new ResponseEntity<Driver>(em,HttpStatus.OK);
 	}
 	
+	
+	//### Update Password of customer ###
+		@PutMapping("/customer/update/")
+		public ResponseEntity<Driver> updateDriverPassword(@RequestParam("mobile") String mobile,
+				@RequestParam("oldpass") String oldpass, @RequestParam("newpass") String newpass)
+				throws  LoginException, DriverException {
+
+			Driver updatedDriver = dservice.updatePassword(mobile, oldpass, newpass);
+
+			return new ResponseEntity<Driver>(updatedDriver, HttpStatus.CREATED);
+
+		}
 	@GetMapping("/bestdriver/{driverId}/{rating}")
-	public List<Driver> getBestDriver(@PathVariable("driverId") Integer driverId,@PathVariable("rating")  float rating) throws DriverException{
+	public List<Driver> getBestDriver(@PathVariable("driverId") Integer driverId,@PathVariable("rating")  float rating) 
+			throws DriverException,LoginException{
 		
 		return dservice.viewBestDriver(driverId,rating);
 	}
